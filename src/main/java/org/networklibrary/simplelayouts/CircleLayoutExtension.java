94a459ba7165756776316e1614dd49897824deed
Georg Summer <georg.summer@gmail.com>
2014-05-21 13:04:54 +0200

    added a Circle Layout. The circle requires doubles so I changed the GridLayoutExtension to also return doubles, so the client side can reuse code.

--- src/main/java/org/networklibrary/simplelayouts/CircleLayoutExtension.java
+++ src/main/java/org/networklibrary/simplelayouts/CircleLayoutExtension.java
@@ -15,49 +15,38 @@ import org.neo4j.server.plugins.ServerPlugin;
 import org.neo4j.server.plugins.Source;
 import org.neo4j.tooling.GlobalGraphOperations;
 
-public class GridLayoutExtension extends ServerPlugin {
+public class CircleLayoutExtension extends ServerPlugin {
 
-	@Name( "gridlayout" )
-	@Description( "calculates a grid layout" )
+	@Name( "circlelayout" )
+	@Description( "calculates a circle layout" )
 	@PluginTarget( GraphDatabaseService.class )
-	public Iterable<Double> gridlayout( @Source GraphDatabaseService graph )
+	public Iterable<Double> circleLayout( @Source GraphDatabaseService graph )
 	{
 		ExecutionEngine engine = new ExecutionEngine( graph );
-		int n = 0;
+		double n = 0;
 
-		int distanceBetweenNodesX = 80; // would normally add the node size here
-		int distanceBetweenNodesY = 40;
 		try (Transaction tx = graph.beginTx()){
 			String query = "match (n) return count(n) as num";
 			ResourceIterator<Long> resultIterator = engine.execute( query).columnAs( "num" );
-			n = new Long(resultIterator.next()).intValue();
+			n = new Long(resultIterator.next()).doubleValue();
 		}
-		List<Double> result = new ArrayList<Double>(n*3);
+		int numEntries = (int) (n*3);
+		List<Double> result = new ArrayList<Double>(numEntries);
 
-		int nCols = (int)Math.ceil(Math.sqrt(n));
-
-		int i = 0;
-		int currCol = 0;
-		int currRow = 0;
-
+		double spaceOfNode = 60;
+		
+		double r = (n * spaceOfNode) / (2*Math.PI);
+		double angle = (2*Math.PI) /  n;
+		
+		double i = 0;
 		try (Transaction tx = graph.beginTx()){
 			for(Node node : GlobalGraphOperations.at(graph).getAllNodes()){
-				if(currCol == nCols){
-					currCol = 0;
-					currRow = currRow + 1;
-				}
-
-				double currX = currCol * distanceBetweenNodesX;
-				double currY = currRow * distanceBetweenNodesY;
-
-				result.add(i, new Long(node.getId()).doubleValue());
-				i=i+1; // go to x
-				result.add(i, currX);
-				i=i+1;
-				result.add(i, currY);
-				i=i+1;
-
-				currCol = currCol + 1;
+				result.add(new Long(node.getId()).doubleValue());
+				double x = r * Math.cos(angle * i);
+				double y = r * Math.sin(angle * i);
+				result.add(x);
+				result.add(y);
+				i = i + 1.0;
 			}
 		}
 
